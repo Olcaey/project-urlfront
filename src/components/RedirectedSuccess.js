5bf111c9759644ff2471af1a97aa39925ae1693c
Kursad Demirtas <69519700+kursademirtas@users.noreply.github.com>
2021-04-21 10:29:37 +0300

    Create RedirectedSuccess.js

---
+++ src/components/RedirectedSuccess.js
@@ -0,0 +1,9 @@
+const Redirect = () => {
+	return (
+		<div className='column_wrapper'>
+			<h2 className='title'>You are being redirected to: <h3 className='subtitle'> twitter.com</h3> </h2>
+			<img src="https://res.cloudinary.com/dlt51mt3f/image/upload/v1618983207/Saly-1_zkutr7.png" alt="3d rocket illustiration" width='300px'/>
+		</div>
+	)
+}
+export default Redirect;
