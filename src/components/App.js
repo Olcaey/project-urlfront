6e873ae2437e32629dcf788fa041008f10c01232
Kursad Demirtas <69519700+kursademirtas@users.noreply.github.com>
2021-04-21 17:05:37 +0300

    query and some logic moved dynamic page.

--- src/components/App.js
+++ src/components/App.js
@@ -1,52 +1,25 @@
 import { useState, useEffect } from "react";
 import CookieModal from "./CookieModal";
-import NotFound from "./NotFound";
 import Redirect from "./RedirectedSuccess";
 import RejectedMessage from "./RejectedMessage";
 import cookie from "js-cookie";
-import { useQuery, gql } from "@apollo/client";
+import { EU_COUNTRIES } from '../core/utils';
 
+const App = ({ data }) => {
 
-const QUERY = gql`
-query publicUrl($shortcode:String) {
-  publicUrl(shortcode:$shortcode) {
-    id
-    fullUrl
-    facebookPixel
-    googleAnalytics
-    country {
-      code
-      country
-    }
-  }
-}
-`;
-
-const App = () => {
-
-	const { data, error } =  useQuery(QUERY, {
-		variables: {
-			shortcode:"q4ro9u3t1z3j",
-		}
-	});
-
-  console.log(data)
-
-  const isUrlValid = true;
-  const isGDPR = true;
+  const userCountry = data.publicUrl.country.code;
+  
+  const isUserFromEU = EU_COUNTRIES.includes(userCountry);
   const [isAcceptedCookies, setIsAcceptedCookies] = useState(false);
   const [isRejected, setIsRejected] = useState(false);
-  
-  if (!isUrlValid) return <NotFound />;
 
   useEffect(() => {
-    if (!isGDPR) {
+    if (!isUserFromEU) {
       setIsAcceptedCookies(true);
     } else {
       setIsAcceptedCookies(cookie.get("ACCEPT_TOKEN"));
     }
   }, []);
-
 
   function setCookie() {
     cookie.set("ACCEPT_TOKEN", "true", { expires: 180 });
@@ -58,7 +31,9 @@ const App = () => {
   }
 
   return isAcceptedCookies ? (
-    <Redirect />
+    <Redirect 
+      URL= {data.publicUrl.fullUrl}
+    />
   ) : isRejected ? (
     <RejectedMessage />
   ) : (
