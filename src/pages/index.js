7281420463d94fbae65aceb02c049b5f302bfb4f
Kursad Demirtas <69519700+kursademirtas@users.noreply.github.com>
2021-04-21 10:30:02 +0300

    some small changes.

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,12 @@
+import App from '../components/App';
 import Layout from '../components/layout';
+
 
 export default function Home() {
 
   return (
     <Layout>
-      CONTENT
+      <App /> 
     </Layout>
   );
 }
