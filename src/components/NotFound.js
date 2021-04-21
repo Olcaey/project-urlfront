b33b6127c344d7f7f48a19bea8d2b65516e10ab2
Kursad Demirtas <69519700+kursademirtas@users.noreply.github.com>
2021-04-21 10:29:35 +0300

    Create NotFound.js

---
+++ src/components/NotFound.js
@@ -0,0 +1,13 @@
+
+
+const NotFound = () => {
+	return (
+		<div className='column_wrapper'>
+			<img src="https://res.cloudinary.com/dlt51mt3f/image/upload/v1618983847/Saly-15_upattr.png" alt="3d young man illustiration" width="300px"/>
+			<h2 className='title'>Url Not Found</h2>
+			<p className='text'>The url you are looking for does not exist. <br/> Please check it again or try another one.Maybe what you are looking for can be found at <a href="">VisitMyPost.com</a></p>
+		</div>
+	)
+}
+
+export default NotFound
