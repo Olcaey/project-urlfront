ded2a81f3af0a6b9709e27ee08c3710b377a4cd0
Kursad Demirtas <69519700+kursademirtas@users.noreply.github.com>
2021-04-21 17:07:04 +0300

    router has been set.

--- src/components/RedirectedSuccess.js
+++ src/components/RedirectedSuccess.js
@@ -1,8 +1,12 @@
+import { useRouter } from 'next/router'
 
-const Redirect = () => {
+const Redirect = ({ URL }) => {
+	const router = useRouter();
+	router.push(URL)
+	
 	return (
 		<div className='column_wrapper'>
-			<h2 className='title'>You are being redirected to: <h3 className='subtitle'> twitter.com</h3> </h2>
+			<h2 className='title'>You are being redirected to: <h3 className='subtitle'> {URL}</h3> </h2>
 			<img src="https://res.cloudinary.com/dlt51mt3f/image/upload/v1618983207/Saly-1_zkutr7.png" alt="3d rocket illustiration" width='300px'/>
 		</div>
 	)
