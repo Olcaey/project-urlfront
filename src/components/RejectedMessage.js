879345b2b1a68a4117265c82475ba053609f7802
Kursad Demirtas <69519700+kursademirtas@users.noreply.github.com>
2021-04-21 10:29:42 +0300

    Create RejectedMessage.js

---
+++ src/components/RejectedMessage.js
@@ -0,0 +1,15 @@
+
+
+const RejectedMessage = () => {
+	return (
+		<div className='column_wrapper'>
+			<img src="https://res.cloudinary.com/dlt51mt3f/image/upload/v1618983537/Saly-11_shafi9.png" alt="3d man illustiration" width='300px'/>
+			<p className='text'>VisitMyPost is a social media and marketing platform enables people to manage their social media and live traffic in an easier way.</p>
+			<p className='text'>Check our website to get to know more</p>
+			<a href="">Go to VisitMyPost.com</a>
+			
+		</div>
+	)
+}
+
+export default RejectedMessage
