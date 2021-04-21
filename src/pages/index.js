4c337cc3a620dd9fccabe07c76318a2992b3b468
Kursad Demirtas <69519700+kursademirtas@users.noreply.github.com>
2021-04-21 13:24:16 +0300

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,42 @@
-import App from '../components/App';
-import Layout from '../components/layout';
+import App from "../components/App";
+import Layout from "../components/layout";
+import { ApolloClient, InMemoryCache, gql } from "@apollo/client";
+import { apiUrl } from "../config";
 
-
-export default function Home() {
-
+export default function Home({ launches }) {
+  console.log(launches);
   return (
     <Layout>
-      <App /> 
+      <App />
     </Layout>
   );
 }
+
+export async function getStaticProps() {
+  const client = new ApolloClient({
+    uri: apiUrl || 'https://hq.visitmypost.com/graphql/',
+    cache: new InMemoryCache(),
+  });
+  const data = await client.query({
+    query: gql`
+      query {
+        publicUrl(shortcode: "q4ro9u3t1z3j") {
+          id
+          fullUrl
+          facebookPixel
+          googleAnalytics
+          country {
+            code
+            country
+          }
+        }
+      }
+    `,
+  });
+  return {
+    props: {
+      data: data,
+    },
+  };
+}
+
