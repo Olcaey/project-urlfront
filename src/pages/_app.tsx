a08a4d30077093a3cffedf8b833c6a82581671e1
Kursad Demirtas <69519700+kursademirtas@users.noreply.github.com>
2021-04-23 10:15:21 +0300

    meta tags added.

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -8,6 +8,7 @@ import { ApolloProvider } from "@apollo/client";
 import client from "../apollo-client";
 import { apiUrl, gtmId, sentryDsn, sentrySampleRate} from "../config";
 import '../global.css'
+import { META_DEFAULTS }from '../core/config'
 
 if (process.env.GTM_ID) {
   TagManager.initialize({ gtmId: gtmId });
@@ -22,6 +23,8 @@ if (sentryDsn) {
   });
 }
 
+const {description, title, type} = META_DEFAULTS;
+
 const App = ({
   Component,
   pageProps
@@ -35,18 +38,18 @@ const App = ({
       <link rel="icon" type="image/png" href="/favicon-36.png" />
       <link rel="icon" type="image/png" href="/safari-pinned-tab.svg" color="#5bbad5" />
       <link rel="manifest" href="/manifest.json" />
-      <meta name="title" content="Meta Tags — Preview, Edit and Generate" />
-      <meta name="description" content="With Meta Tags you can edit and experiment with your content then preview how your webpage will look on Google, Facebook, Twitter and more!"/>
-      <meta property="og:type" content="website"/>
+      <meta name="title" content={title} />
+      <meta name="description" content={description}/>
+      <meta property="og:type" content={type}/>
       <meta property="og:url" content="https://metatags.io/"/>
-      <meta property="og:title" content="Meta Tags — Preview, Edit and Generate"/>
-      <meta property="og:description" content="With Meta Tags you can edit and experiment with your content then preview how your webpage will look on Google, Facebook, Twitter and more!"/>
-      <meta property="og:image" content="https://metatags.io/assets/meta-tags-16a33a6a8531e519cc0936fbba0ad904e52d35f34a46c97a2c9f6f7dd7d336f2.png"/>
+      <meta property="og:title" content={title} />
+      <meta property="og:description"  content={description}/>
+      <meta property="og:image" content="/favicon-36.png"/>
       <meta property="twitter:card" content="summary_large_image"/>
       <meta property="twitter:url" content="https://metatags.io/"/>
-      <meta property="twitter:title" content="Meta Tags — Preview, Edit and Generate"/>
-      <meta property="twitter:description" content="With Meta Tags you can edit and experiment with your content then preview how your webpage will look on Google, Facebook, Twitter and more!"/>
-      <meta property="twitter:image" content="https://metatags.io/assets/meta-tags-16a33a6a8531e519cc0936fbba0ad904e52d35f34a46c97a2c9f6f7dd7d336f2.png"/>
+      <meta property="twitter:title" content={title} />
+      <meta property="twitter:description" content={description}/>
+      <meta property="twitter:image" content="/favicon-36.png"/>
     </Head>
 
     <Component {...pageProps} />
